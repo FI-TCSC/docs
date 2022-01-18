e9a98d40c1df05eed270b571e5134e2d53554a9b
Mika Anttila <16833294+mantti@users.noreply.github.com>
2022-01-18 13:53:22 +0200

    Testing other theme

--- conf.py
+++ conf.py
@@ -88,7 +88,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -96,7 +96,9 @@ html_theme = 'classic'
 #
 # This doesn't work yet
 # html_theme_options = {'globaltoc_includehidden': 'True',}
-
+html_theme_options = {
+	'show_relbars': 'False',
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
@@ -108,8 +110,8 @@ html_static_path = ['_static']
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
 html_sidebars = {
-    '**': [ 'globaltoc.html', 'localtoc.html',
-       #  'general.html',  # needs 'show_related': True theme option to display
+    '**': [ 'about.html', 'globaltoc.html', 
+        # 'general.html',  # needs 'show_related': True theme option to display
     ]
 }
 
